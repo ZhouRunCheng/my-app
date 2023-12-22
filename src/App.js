d9bfb5846e16c772813e9ac2ae6db9e16b439e72
raincoat <raincoat_zhow@163.com>
2023-12-22 12:13:05 +0800

    页面布局

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react'
+
+const images = [
+    {
+      src: "https://img0.baidu.com/it/u=2238321549,582030440&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500",
+      caption: "loren"
+    },
+    {
+      src: "https://img0.baidu.com/it/u=3563370327,636231767&fm=253&fmt=auto&app=120&f=JPEG?w=1280&h=800",
+      caption: "图片2的配文"
+    },
+    {
+      src: "https://up.deskcity.org/pic_source/27/e1/d0/27e1d0a3ba084f810fdc33469491259a.jpg",
+      caption: "图片3的配文"
+    }
+  ];
 
 function App() {
-  const [value,setValue]= useState();
-  const [list,setList]= useState([]);
-  useEffect(() => {
-    console.log('val', value)
-    setTimeout(() => {
-      setValue('2')
-    }, 1000)
-  }, [value])
   return (
-      <span>
-          <input
-              onChange={(e)=>{
-                  setValue(e.target.value)
-              }}
-              value={value}
-          />
-          {
-              list.map((elem) => {
-                  return (<span key={elem.name}>{elem.name}</span>);
-              })
-          }
-      </span>);
+    <div className="container">
+        {
+            images.map((image, index) => (
+                <div className="image-container" key={index}>
+                    <img className="image" src={image.src} alt="配图" />
+                    <p className="caption">{image.caption}</p>
+                </div>
+            ))
+        }
+    </div>
+  )
 }
 
 export default App;
